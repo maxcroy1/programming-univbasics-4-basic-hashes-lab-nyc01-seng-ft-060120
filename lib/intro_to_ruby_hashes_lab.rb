62388823de93bc505e23372de84e92baac460f32
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 17:09:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,6 +6,11 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+   my_family = {
+    dad: "Jeff",
+    mom: "Jennifer",
+    sister: "Samantha"
+  }
 end
 
 def pioneer
